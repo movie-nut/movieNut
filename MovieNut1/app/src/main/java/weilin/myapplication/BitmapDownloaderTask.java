1e6b12e6db6445c9b525fe8e0e3c224bd33ac682
Aw Wei Lin <weinilinhappy@gmail.com>
2015-07-06 12:54:41 +0800

    debug the slow loading of image

---
+++ MovieNut1/app/src/main/java/weilin/myapplication/BitmapDownloaderTask.java
@@ -0,0 +1,184 @@
+package weilin.myapplication;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+import android.widget.ImageView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by WeiLin on 5/7/15.
+ */
+
+class BitmapDownloaderTask extends AsyncTask<String, Void, Bitmap> {
+
+    private final WeakReference<ImageView> imageViewReference;
+    private String url;
+
+    public BitmapDownloaderTask(ImageView imageView) {
+        imageViewReference = new WeakReference<>(imageView);
+    }
+
+    public static void download(String url, ImageView imageView) {
+        if (cancelPotentialDownload(url, imageView)) {
+            BitmapDownloaderTask task = new BitmapDownloaderTask(imageView);
+            DownloadedDrawable downloadedDrawable = new DownloadedDrawable(task);
+            imageView.setImageDrawable(downloadedDrawable);
+
+            task.execute(url);
+        }
+    }
+
+    @Override
+    // Actual download method, run in the task thread
+    protected Bitmap doInBackground(String... params) {
+        // params comes from the execute() call: params[0] is the url.
+        return downloadBitmap(params[0]);
+    }
+
+
+    @Override
+    // Once the image is downloaded, associates it to the imageView
+    protected void onPostExecute(Bitmap bitmap) {
+        if (imageViewReference != null) {
+            ImageView imageView = imageViewReference.get();
+            BitmapDownloaderTask bitmapDownloaderTask = getBitmapDownloaderTask(imageView);
+            // Change bitmap only if this process is still associated with it
+            if (this == bitmapDownloaderTask) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private static boolean cancelPotentialDownload(String url, ImageView imageView) {
+        BitmapDownloaderTask bitmapDownloaderTask = getBitmapDownloaderTask(imageView);
+
+        if (bitmapDownloaderTask != null) {
+            String bitmapUrl = bitmapDownloaderTask.url;
+            if ((bitmapUrl == null) || (!bitmapUrl.equals(url))) {
+                bitmapDownloaderTask.cancel(true);
+            } else {
+                // The same URL is already being downloaded.
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static BitmapDownloaderTask getBitmapDownloaderTask(ImageView imageView) {
+        if (imageView != null) {
+            Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof DownloadedDrawable) {
+                DownloadedDrawable downloadedDrawable = (DownloadedDrawable) drawable;
+                return downloadedDrawable.getBitmapDownloaderTask();
+            }
+        }
+        return null;
+    }
+
+    static class DownloadedDrawable extends ColorDrawable {
+        private final WeakReference<BitmapDownloaderTask> bitmapDownloaderTaskReference;
+
+        public DownloadedDrawable(BitmapDownloaderTask bitmapDownloaderTask) {
+            super(Color.BLACK);
+            bitmapDownloaderTaskReference =
+                    new WeakReference<BitmapDownloaderTask>(bitmapDownloaderTask);
+        }
+
+        public BitmapDownloaderTask getBitmapDownloaderTask() {
+            return bitmapDownloaderTaskReference.get();
+        }
+    }
+
+    static Bitmap downloadBitmap(String url) {
+
+        try {
+            // Decode image size
+            URL urlFINAL = new URL(url);
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(urlFINAL.openConnection().getInputStream(), null, o);
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 70;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+
+            return BitmapFactory.decodeStream(urlFINAL.openConnection().getInputStream(), null, o2);
+        } catch (FileNotFoundException e) {
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+        /*
+        final AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
+        final HttpGet getRequest = new HttpGet(url);
+
+        try {
+            HttpResponse response = client.execute(getRequest);
+            final int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                Log.w("ImageDownloader", "Error " + statusCode + " while retrieving bitmap from " + url);
+                return null;
+            }
+
+            final HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream inputStream = null;
+                try {
+                    inputStream = entity.getContent();
+                    final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    return bitmap;
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    entity.consumeContent();
+                }
+            }
+        } catch (Exception e) {
+            // Could provide a more explicit error message for IOException or IllegalStateException
+            getRequest.abort();
+            final int imageDownloader = Log.w("ImageDownloader", "Error while retrieving bitmap from ", e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return null;
+    }
+*/
--- MovieNut1/app/src/main/java/weilin/myapplication/DisplayResults.java
+++ MovieNut1/app/src/main/java/weilin/myapplication/DisplayResults.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,16 +17,17 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
+import java.util.HashMap;
 
 public class DisplayResults extends Activity {
     String[] moviesInfo;
@@ -33,14 +35,16 @@ public class DisplayResults extends Activity {
     String[] image;
     String[] releaseDates;
     ArrayList<Movies> movies = new ArrayList<>();
+    private HashMap<String, Bitmap> pictures = new HashMap<>();
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)  {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_results);
 
         getMoviesInfo();
+        //  getImageStored();
 
         ListView list = (ListView) findViewById(R.id.listView);
         moviesAdapter adapter = new moviesAdapter(this, moviesInfo, description, image);
@@ -74,7 +78,7 @@ public class DisplayResults extends Activity {
     }
 
     private void storeBackIntoString() {
-        for(int i = 0; i < movies.size(); i++){
+        for (int i = 0; i < movies.size(); i++) {
             moviesInfo[i + 1] = movies.get(i).getMovieTitle();
             description[i + 1] = movies.get(i).getDescription();
             image[i + 1] = movies.get(i).getImageURL();
@@ -83,7 +87,7 @@ public class DisplayResults extends Activity {
 
     private void storeInMovieClass() throws ParseException {
 
-        for(int i = 1; i < moviesInfo.length; i++){
+        for (int i = 1; i < moviesInfo.length; i++) {
             movies.add(new Movies());
             movies.get(i - 1).setMovieTitle(moviesInfo[i]);
             movies.get(i - 1).setDecription(description[i]);
@@ -92,69 +96,144 @@ public class DisplayResults extends Activity {
         }
     }
 
-    class moviesAdapter extends ArrayAdapter<String> {
-    Context context;
-    String[] moviesInfo;
-    String[] description;
-    String[] image;
-
-    //put images int image[]
-    moviesAdapter(Context c, String[] moviesInfo, String[] description, String[] image){
-        super(c, R.layout.single_row, R.id.textView3,moviesInfo);
-        this.context = c;
-        this.moviesInfo = moviesInfo;
-        this.description = description;
-        this.image = image;
+    public void getImageStored() {
+        //  if()
     }
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        View row = inflater.inflate(R.layout.single_row, parent, false);
-      ImageView myImage = (ImageView) row.findViewById(R.id.imageView);
-        TextView movieTitles = (TextView) row.findViewById(R.id.textView3);
-        TextView myDescription = (TextView) row.findViewById(R.id.textView4);
+    class moviesAdapter extends ArrayAdapter<String> implements weilin.myapplication.moviesAdapter {
+        Context context;
+        String[] moviesInfo;
+        String[] description;
+        String[] image;
 
-        movieTitles.setText(moviesInfo[position]);
-        myDescription.setText(description[position]);
+        //put images int image[]
+        moviesAdapter(Context c, String[] moviesInfo, String[] description, String[] image) {
+            super(c, R.layout.single_row, R.id.textView3, moviesInfo);
+            this.context = c;
+            this.moviesInfo = moviesInfo;
+            this.description = description;
+            this.image = image;
+        }
 
-        URL url = null;
-      if(image.length > position && (!image[position].equals("") || !(image[position] == null))) {
-          try {
-              url = new URL(image[position]);
-          } catch (MalformedURLException e) {
-              e.printStackTrace();
-          }
-          Bitmap bmp = null;
-          try {
-              if(url != null){
-                  bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-              }
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-          myImage.setImageBitmap(bmp);
-      }
-        return row;
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = inflater.inflate(R.layout.single_row, parent, false);
+            ImageView myImage = (ImageView) row.findViewById(R.id.imageView);
+            TextView movieTitles = (TextView) row.findViewById(R.id.textView3);
+            TextView myDescription = (TextView) row.findViewById(R.id.textView4);
+
+            movieTitles.setText(moviesInfo[position]);
+            myDescription.setText(description[position]);
+
+            if (image.length > position && (!image[position].equals("") || !(image[position] == null))) {
+                Bitmap bmp;
+//                ImageView view = (ImageView) convertView;
+  //              if (view == null) {
+    //                view = new ImageView(context);
+      //          }
+                String url = null;
+                try {
+                    url = new URL(image[position]).toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+               // Picasso.with(context).load(url).into(myImage);
+                Picasso.with(context)
+                        .load(url)
+                        .resize(50, 50)
+                        .centerCrop()
+                        .into(myImage);
+            }
+
+        /*
+              if (image[position] == null
+                      || pictures.get(image[position]) == null) {
+                  BitmapFactory.Options options = new BitmapFactory.Options();
+                  options.inDither = false;                     //Disable Dithering mode
+                  options.inPurgeable = true;                   //Tell to gc that whether it needs free memory, the Bitmap can be cleared
+                  options.inInputShareable = true;              //Which kind of reference will be used to recover the Bitmap data after being clear, when it will be used in the future
+                  options.inTempStorage = new byte[32 * 1024];
+                  options.inSampleSize = 4;
+
+
+                  URL url = null;
+                  try {
+                      url = new URL(image[position]);
+                  } catch (MalformedURLException e) {
+                      e.printStackTrace();
+                  }
+
+                  //1.pop
+                  BitmapDownloaderTask.download(image[position], myImage);
+                  BitmapDownloaderTask task = new BitmapDownloaderTask(myImage);
+                  task.execute(image[position]);
+
+
+                  //other
+                  ImageLoader imageLoader = new ImageLoader(getApplicationContext());
+                  myImage.setTag(image[position]);
+                  imageLoader.DisplayImage(image[position], DisplayResults.this, myImage);
+*/
+
+
+
+    /* class BitmapDownloaderTask, see below */
+
+            //    bmp = decodeFile(image[position]);
+            //  pictures.put(image[position], bmp);
+
+            // else {
+            //    bmp = pictures.get(image[position]);
+
+
+            //   myImage.setImageBitmap(bmp);
+
+            return row;
+        }
+
+
+        private Bitmap decodeFile(String f) {
+            try {
+                // Decode image size
+                URL url = new URL(f);
+                BitmapFactory.Options o = new BitmapFactory.Options();
+                o.inJustDecodeBounds = true;
+                BitmapFactory.decodeStream(url.openConnection().getInputStream(), null, o);
+
+                // The new size we want to scale to
+                final int REQUIRED_SIZE = 70;
+
+                // Find the correct scale value. It should be the power of 2.
+                int scale = 1;
+                while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                        o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                    scale *= 2;
+                }
+
+                // Decode with inSampleSize
+                BitmapFactory.Options o2 = new BitmapFactory.Options();
+                o2.inSampleSize = scale;
+
+                return BitmapFactory.decodeStream(url.openConnection().getInputStream(), null, o2);
+            } catch (FileNotFoundException e) {
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
-}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_display_results, menu);
         return true;
     }
-/*
-    private final void focusOnButtons(){
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ScrollView sv = (ScrollView)findViewById(R.id.scrl);
-                sv.scrollTo(0, sv.getBottom());
-            }
-        },1000);
-    }
-*/
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -169,7 +248,51 @@ public class DisplayResults extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
 }
+
+
+/*
+    boolean canUseForInBitmap(
+            Bitmap candidate, BitmapFactory.Options targetOptions) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // From Android 4.4 (KitKat) onward we can re-use if the byte size of
+            // the new bitmap is smaller than the reusable bitmap candidate
+            // allocation byte count.
+            int width = targetOptions.outWidth / targetOptions.inSampleSize;
+            int height = targetOptions.outHeight / targetOptions.inSampleSize;
+            int byteCount = width * height * getBytesPerPixel(candidate.getConfig());
+            return byteCount <= candidate.getAllocationByteCount();
+        }
+
+        // On earlier versions, the dimensions must match exactly and the inSampleSize must be 1
+        return candidate.getWidth() == targetOptions.outWidth
+                && candidate.getHeight() == targetOptions.outHeight
+                && targetOptions.inSampleSize == 1;
+    }
+        */
+
+        /**
+         * A helper function to return the byte usage per pixel of a bitmap based on its configuration.
+         */
+    /*
+    static int getBytesPerPixel(Bitmap.Config config) {
+        if (config == Bitmap.Config.ARGB_8888) {
+            return 4;
+        } else if (config == Bitmap.Config.RGB_565) {
+            return 2;
+        } else if (config == Bitmap.Config.ARGB_4444) {
+            return 2;
+        } else if (config == Bitmap.Config.ALPHA_8) {
+            return 1;
+        }
+        return 1;
+    }
+*/
+
+
+
+
+
+
+
